c90f41a3d08a49cf2450bb537ab8522a1a606524
pvykhrov <ekreative.teaching@gmail.com>
2023-03-10 15:16:27 +0200

    3

--- Program.cs
+++ Program.cs
@@ -13,41 +13,36 @@ namespace FootballSearchEngine
             Console.WriteLine("How do you want to search for information:");
             Console.WriteLine("1 - Sort by years");
             Console.WriteLine("2 - Sort by country of the winners");
-            Console.WriteLine("3 - Sort by countries in which the matches were held");
+            Console.WriteLine("3 - Take all of FIFA World Cup ");
 
             //Заносимо данні в масив: рік, переможець, місто
-            //test commit MacOS
             string[,] worldCups = new string[,]
             {
-                { "1930", "Uruguay", "Montevideo" },
-                { "1934", "Italy", "Rome" },
-                { "1938", "Italy", "Paris" },
-                { "1950", "Uruguay", "Rio de Janeiro" },
-                { "1954", "West Germany", "Bern" },
-                { "1958", "Brazil", "Stockholm" },
-                { "1962", "Brazil", "Santiago" },
-                { "1966", "England", "London" },
-                { "1970", "Brazil", "Mexico City" },
-                { "1974", "West Germany", "Munich" },
-                { "1978", "Argentina", "Buenos Aires" },
-                { "1982", "Italy", "Madrid" },
-                { "1986", "Argentina", "Mexico City" },
-                { "1990", "West Germany", "Rome" },
-                { "1994", "Brazil", "Pasadena" },
-                { "1998", "France", "Saint-Denis" },
-                { "2002", "Brazil", "Yokohama" },
-                { "2006", "Italy", "Berlin" },
-                { "2010", "Spain", "Johannesburg" },
-                { "2014", "Germany", "Rio de Janeiro" },
-                { "2018", "France", "Moscow" },
-                { "2022", "Argentina", "Katar" },
-
+                { "1930", "Uruguay", "Montevideo", "4-2" },
+                { "1934", "Italy", "Rome", "2-1 (a.e.t.)" },
+                { "1938", "Italy", "Paris", "4-2" },
+                { "1950", "Uruguay", "Rio de Janeiro", "2-1" },
+                { "1954", "West Germany", "Bern", "3-2" },
+                { "1958", "Brazil", "Stockholm", "5-2" },
+                { "1962", "Brazil", "Santiago",  "3-1" },
+                { "1966", "England", "London", "4-2 (a.e.t.)" },
+                { "1970", "Brazil", "Mexico", "4-1" },
+                { "1974", "West Germany", "Munich", "2-1" },
+                { "1978", "Argentina", "Buenos Aires", "3-1 (a.e.t.)" },
+                { "1982", "Italy", "Madrid", "3-1" },
+                { "1986", "Argentina", "Mexico", "3-2" },
+                { "1990", "West Germany", "Rome", "1-0" },
+                { "1994", "Brazil", "Pasadena", "0-0 (a.e.t.)" },
+                { "1998", "France", "Saint-Denis", "3-0" },
+                { "2002", "Brazil", "Yokohama", "2-0" },
+                { "2006", "Italy", "Berlin", "1-1 (a.e.t.)" },
+                { "2010", "Spain", "Johannesburg", "1-0 (a.e.t.)"},
+                { "2014", "Germany", "Rio de Janeiro", "1-0 (a.e.t.)" },
+                { "2018", "France", "Moscow", "4-2" },
+                { "2022", "Argentina", "Lusail", "3-3" },
             };
 
             // Виконання операцій залежно від вибраної опції пошуку
-           
-            
-            
             int searchOption = int.Parse(Console.ReadLine());
             switch (searchOption)
             {
@@ -94,10 +89,10 @@ namespace FootballSearchEngine
 
                     for (int i = 0; i < worldCups.GetLength(0); i++)
                     {
-                        countries.Add(worldCups[i, 1]);
+                        countries.Add($"{worldCups[i, 0]}\tin\t{worldCups[i, 2]}\t \t winner\t{worldCups[i, 1]}\t\t{worldCups[i, 3]}");
                     }
 
-                    Console.WriteLine("Countries where the matches were held:");
+                    Console.WriteLine("City where the matches were held:");
                     foreach (string c in countries)
                     {
                         Console.WriteLine(c);
